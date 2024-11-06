6262a11625af1e62ed58cc4447467858009327ea
Bluecookies05 <98850306+Bluecookies05@users.noreply.github.com>
2024-11-06 16:35:00 -0500

    almost done with lab 1

---
+++ lab5.py
@@ -0,0 +1,31 @@
+## ============= grade calculator lab 1 ================
+def lab_1():
+    num= int(input("enter number of scores:  "))
+    scores=[]
+    for i in range (num):
+        score=int(input("Enter the score:  "))
+        scores.append(score)
+print(scores)
+avg=sum(score)/len(score)
+
+if avg>=90:
+    print("A")
+elif avg>=80 and avg<90:
+    print("B")
+elif avg>=70 and avg<80:
+    print("C")
+elif avg>=60 and avg<70:
+    print("D")
+else avg>=0 and avg<60:
+    print("F")
+
+## ============= grade calculator lab 1 ================
+
+
+## ============= Even and odd number counter lab 2 ==================
+
+
+## ============= Even and odd number counter lab 2 ==================
+
+
+## ============ Unique list merger lab 3 =========================
