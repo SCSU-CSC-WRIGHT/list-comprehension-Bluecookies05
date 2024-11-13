9e27e96fc99c47bd765abe540b129d5696de3711
Bluecookies05 <98850306+Bluecookies05@users.noreply.github.com>
2024-11-13 01:24:18 +0000

    fixing lab 1 and 2

--- lab5.py
+++ lab5.py
@@ -1,32 +1,54 @@
 ## ============= grade calculator lab 1 ================
 def lab_1():
-    num= int(input("enter number of scores:  "))
-    scores=[]
-    for i in range (num):
-        score=int(input("Enter the score:  "))
-        scores.append(score)
-print(scores)
-avg=sum(scores)/len(scores)
+    num = int(input("Enter number of scores: "))
+    scores = []
 
-if avg>=90:
-    print("A")
-elif avg>=80 and avg<90:
-    print("B")
-elif avg>=70 and avg<80:
-    print("C")
-elif avg>=60 and avg<70:
-    print("D")
-else avg>=0 and avg<60:
-    print("F")
+    for i in range(num):
+        score = int(input("Enter the score: "))
+        scores.append(score)
+
+    print("Scores:", scores)
+    avg = sum(scores) / len(scores)
+
+    if avg >= 90:
+        print("A")
+    elif avg >= 80:
+        print("B")
+    elif avg >= 70:
+        print("C")
+    elif avg >= 60:
+        print("D")
+    else:
+        print("F")
 
 lab_1()
 
-## ============= Even and odd number counter lab 2 ==================
-
-
-def lab_2 = split.()
 
 ## ============= Even and odd number counter lab 2 ==================
+
+def lab_2():
+    numbers = input("Enter numbers separated by spaces: ").split()
+    numbers = [int(num) for num in numbers]  
+
+    even_count = 0
+    odd_count = 0
+    even_numbers = []
+    odd_numbers = []
+
+    for num in numbers:
+        if num % 2 == 0:
+            even_count += 1
+            even_numbers.append(num)
+        else:
+            odd_count += 1
+            odd_numbers.append(num)
+
+    print("Here are the even numbers:", even_numbers)
+    print("Here are the odd numbers:", odd_numbers)
+    print("Even numbers count:", even_count)
+    print("Odd numbers count:", odd_count)
+
+lab_2()
 
 
 ## ============ Unique list merger lab 3 =========================
